97ed174add471fbeb8adb51bd8d35fc887a78573
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2021-02-26 10:30:26 -0800

    Changed license of all Operator code to UPL
    Change-Id: Id2896763b0eb88c20cc15dd4452ed1f0a581c954

---
+++ README.md
@@ -0,0 +1,13 @@
+Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+
+This is a release of MySQL Operator, a Kubernetes Operator for MySQL InnoDB Cluster
+
+License information can be found in the LICENSE-operator file.
+This distribution may include materials developed by third parties. For license
+and attribution notices for these materials, please refer to the LICENSE-operator file.
+
+For more information on MySQL Operator visit http://dev.mysql.com/doc/operator
+For additional downloads and the source of MySQL Operator visit http://dev.mysql.com/downloads
+
+MySQL Operator is brought to you by the MySQL team at Oracle.
+
--- mysqloperator/controller/consts.py
+++ mysqloperator/controller/consts.py
@@ -1,23 +1,7 @@
-# Copyright (c) 2020, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2021, Oracle and/or its affiliates.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2.0,
-# as published by the Free Software Foundation.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
-# This program is also distributed with certain software (including
-# but not limited to OpenSSL) that is licensed under separate terms, as
-# designated in a particular file or component or in included license
-# documentation.  The authors of MySQL hereby grant you an additional
-# permission to link the program and your derivative works with the
-# separately licensed software that they have included with MySQL.
-# This program is distributed in the hope that it will be useful,  but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-# the GNU General Public License, version 2.0, for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 GROUP = "mysql.oracle.com"
 VERSION = "v2alpha1"

9d1e1e602beea4a734b42d9f45216a7de5201137
Johannes Schlüter <johannes.schlueter@oracle.com>
2021-04-13 23:24:47 +0200

    Fix typo and copy&paste error

--- deploy/patch_deployment.py
+++ deploy/patch_deployment.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-i# Copyright (c) 2021, Oracle and/or its affiliates.
+# Copyright (c) 2021, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #

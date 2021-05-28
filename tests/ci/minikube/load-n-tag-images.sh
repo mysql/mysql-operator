8399d2a8d5adb794f650a6ede7666c28f7e61a90
Johannes Schlüter <johannes.schlueter@oracle.com>
2021-05-28 13:50:11 +0200

    Update header

--- tests/ci/minikube/load-n-tag-images.sh
+++ tests/ci/minikube/load-n-tag-images.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2020, 2021, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+#
+
 
 # ensure minikube uses proper k8s-shell image
 # we've noticed that 'minikube image load' may work weirdly when another image with

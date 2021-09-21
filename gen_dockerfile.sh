ad8a696ca51ab737d051de134d68f4585078455f
Lars Tangvald <lars.tangvald@oracle.com>
2021-09-21 11:05:06 +0200

    Docker: Separate versions from names.
    Should make it easier to make it more dynamic later

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -5,17 +5,18 @@
 
 #!/bin/bash
 
-MYSQL_OPERATOR_PYTHON_DEPS="mysql-operator-python-deps:3.7.7"
-MYSQL_SHELL_PACKAGE="mysql-shell-8.0.26"
 MYSQL_REPO_URL="http://repo.mysql.com"
+MYSQL_OPERATOR_PYTHON_DEPS="mysql-operator-python-deps"
+MYSQL_OPERATOR_PYTHON_DEPS_VERSION="3.9.5"
+MYSQL_SHELL_VERSION=8.0.26
 if [ -n "${1}" ]; then
   MYSQL_REPO_URL="${1}"
 fi
 if [ -n "${2}" ]; then
   MYSQL_OPERATOR_PYTHON_DEPS="${2}"
 fi
-sed 's#%%MYSQL_OPERATOR_PYTHON_DEPS%%#'"${MYSQL_OPERATOR_PYTHON_DEPS}"'#g' docker-build/Dockerfile > tmpfile
-sed -i 's#%%MYSQL_SHELL_PACKAGE%%#'"${MYSQL_SHELL_PACKAGE}"'#g' tmpfile
+sed 's#%%MYSQL_OPERATOR_PYTHON_DEPS%%#'"${MYSQL_OPERATOR_PYTHON_DEPS}:${MYSQL_OPERATOR_PYTHON_DEPS_VERSION}"'#g' docker-build/Dockerfile > tmpfile
+sed -i 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
 sed -i 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
 
 mv tmpfile Dockerfile

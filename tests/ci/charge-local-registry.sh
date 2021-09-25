24b598cd6f14e8100415dc9a4738c1261939bbaa
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2021-09-25 18:28:07 +0200

    MYF-250: change jenkins setup to use a local registry
    - changes in ./tests/ci/charge-local-registry.sh and list of images

--- tests/ci/charge-local-registry.sh
+++ tests/ci/charge-local-registry.sh
@@ -5,27 +5,32 @@
 #
 
 # charge local registry
-# usage: [--pull] [--no-pull] [--push-only] [--pull-only] <registry-url> <repository-name> <images-list>
-# registry-url: e.g. registry.localhost:5000
-# repository-name: e.g. mysql, jenkins, ...
+# usage: [--pull] [--no-pull] [--push-only] [--pull-only] \
+#	[pull-registry-url] <pull-repository-name> <push-registry-url> <push-repository-name> <images-list>
+# pull-registry-url: the remote registry to pull the images from, e.g. our internal development mysql
+#	repos, it may be an empty string, then the default docker hub (docker.io) will be used
+# pull-repository-name: the remote repository to pull the images from, e.g. qa, ...
+# push-registry-url: the local registry to push the images to, e.g. registry.localhost:5000
+# push-repository-name: the local repository to push the images to, e.g. mysql, jenkins, ...
 # --pull: force to pull all images from list
 # --no-pull/--push-only: force to not pull any image from list, only push images to the local registry
 # --pull-only: pull images with default policy, but don't push to the local registry (may be
 #   needed when e.g. VPN is required to pull images, but it precludes pushing to a local registry)
+# images-list: input file with list of image names to pull from the remote/push to the local registry
 # e.g.: --no-pull registry.localhost:5000 mysql ./images-list.txt
+#
 # input sample, every three lines are in order
 # DEST_IMAGE_NAME the name of the image in the local repo (e.g. mysql-server:8.0.24)
-# SRC_IMAGE_FULL_NAME source image, potentially to be pulled (e.g. mysql/mysql-server:8.0.24 or
-# mydocker.mysql.oraclecorp.com/qa/mysql-router:8.0.25)
+# SRC_IMAGE_NAME source image, potentially to be pulled (e.g. mysql-server:8.0.24)
 # PULL_POLICY for the source image (0 - not to pull, 1 - to pull)
 # separator ---
 # <images-list.txt>
 # mysql-server:8.0.24
-# mysql/mysql-server:8.0.24
+# mysql-server:8.0.24
 # 1
 # ---
-# mysql-router:8.0.24
-# mysql/mysql-router:8.0.24
+# mysql-operator-commercial:8.0.25-2.0.1
+# mysql-operator:8.0.25-2.0.1
 # 1
 # ---
 # [...]
@@ -57,21 +62,24 @@ while [[ $# -gt 0 ]]; do
 	esac
 done
 
-if [ "$#" -ne 3 ]; then
-	echo "usage: [--pull] [--no-pull] [--push-only] [--pull-only] <registry-url> <repository-name> <images-list>"
+if [ "$#" -ne 5 ]; then
+	echo "usage: [--pull] [--no-pull] [--push-only] [--pull-only] [pull-registry-url] " \
+		"<pull-repository-name> <push-registry-url> <push-repository-name> <images-list>"
 	exit 1
 fi
 
-REGISTRY_URL=$1
-REPOSITORY_NAME=$2
-IMAGES_LIST=$3
+PULL_REGISTRY_URL=$1
+PULL_REPOSITORY_NAME=$2
+PUSH_REGISTRY_URL=$3
+PUSH_REPOSITORY_NAME=$4
+IMAGES_LIST=$5
 
 images_to_process=$(mktemp)
-cat "$3" | awk 'BEGIN { RS = "---" } { print $1 " " $2 " " $3 }' > $images_to_process
+cat "$IMAGES_LIST" | awk 'BEGIN { RS = "---" } { print $1 " " $2 " " $3 }' > $images_to_process
 
 while read -r image_info
 do
-	read DEST_IMAGE_NAME SRC_IMAGE_FULL_NAME PULL_IMAGE <<< "$image_info"
+	read DEST_IMAGE_NAME SRC_IMAGE_NAME PULL_IMAGE <<< "$image_info"
 	case ${PULL_POLICY} in
 		"default")
 			PERFORM_PULL=$PULL_IMAGE
@@ -88,15 +96,20 @@ do
 	esac
 
 	if [ $PERFORM_PULL -eq 1 ]; then
-		docker pull $SRC_IMAGE_FULL_NAME
+		if [ -n $PULL_REGISTRY_URL ]; then
+			SRC_IMAGE=$PULL_REGISTRY_URL/$PULL_REPOSITORY_NAME/$SRC_IMAGE_NAME
+		else
+			SRC_IMAGE=$PULL_REPOSITORY_NAME/$SRC_IMAGE_NAME
+		fi
+		docker pull $SRC_IMAGE
 	fi
 
 	if [ $PUSH_POLICY == "default" ]; then
-		FULL_IMAGE_NAME=$REGISTRY_URL/$REPOSITORY_NAME/$DEST_IMAGE_NAME
+		DEST_IMAGE=$PUSH_REGISTRY_URL/$PUSH_REPOSITORY_NAME/$DEST_IMAGE_NAME
 
-		docker tag $SRC_IMAGE_FULL_NAME $FULL_IMAGE_NAME
-		docker push $FULL_IMAGE_NAME
-		docker image rm $FULL_IMAGE_NAME
+		docker tag $SRC_IMAGE $DEST_IMAGE
+		docker push $DEST_IMAGE
+		docker image rm $DEST_IMAGE
 	fi
 done < $images_to_process
 

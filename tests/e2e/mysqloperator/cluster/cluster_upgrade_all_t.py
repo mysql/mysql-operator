690bd71ce5b580c7c52f8135c43c60cad342ad61
Lars Tangvald <lars.tangvald@oracle.com>
2021-10-07 10:10:10 +0200

    Setup Helm Repository in Github Pages

--- tests/e2e/mysqloperator/cluster/cluster_upgrade_all_t.py
+++
@@ -1,5 +0,0 @@
-# test uprgade from all supported versions
-# Copyright (c) 2020, Oracle and/or its affiliates.
-#
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-#
--- tests/utils/ote/minishift.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2020, Oracle and/or its affiliates.
-#
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
-#
-
-from .base import BaseEnvironment
-
-class MinishiftEnvironment(BaseEnvironment):
-    pass

45abbd60dcf3363f1aba48ba5635d1948169ce46
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2021-11-21 23:20:13 -0800

    Fixes MySQLInteractivePodSession
    Fixes issue in MySQLInteractivePodSession where
    output from shell would sometimes be truncated.
    
    Fixes random failures in Cluster3Defaults.test_1_router
    
    Change-Id: I23579c4107371c30424280db417a18a6a8dbadfd

--- mysqloperator/controller/innodbcluster/cluster_api.py
+++ mysqloperator/controller/innodbcluster/cluster_api.py
@@ -157,7 +157,10 @@ class InnoDBClusterSpec:
     baseServerId: int
     # override volumeClaimTemplates for datadir in MySQL pods (optional)
     datadirVolumeClaimTemplate = None
-    # additional MySQL configuration options
+    # config options
+    mysqlOptions: dict = {}
+
+    # additional raw MySQL configuration options
     mycnf: str = ""
     # override pod template for MySQL (optional)
     podSpec: dict = {}
@@ -228,6 +231,9 @@ class InnoDBClusterSpec:
 
         if "datadirVolumeClaimTemplate" in spec:
             self.datadirVolumeClaimTemplate = spec.get("datadirVolumeClaimTemplate")
+
+        if "mysqlOptions" in spec:
+            self.mysqlOptions = dget_dict(spec, "mysqlOptions", "spec")
 
         if "mycnf" in spec:
             self.mycnf = dget_str(spec, "mycnf", "spec")

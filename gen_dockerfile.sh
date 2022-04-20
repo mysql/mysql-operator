ef596af0d8ba5c0dbbfaa3e4c009b0c9fccf66a3
Lars Tangvald <lars.tangvald@oracle.com>
2022-04-20 09:56:04 +0200

    Allow dynamic shell version and config rpm for Dockerfile generation

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -9,14 +9,22 @@ MYSQL_REPO_URL="http://repo.mysql.com"
 MYSQL_OPERATOR_PYTHON_DEPS="mysql-operator-python-deps"
 MYSQL_OPERATOR_PYTHON_DEPS_VERSION="3.9.5"
 MYSQL_SHELL_VERSION=8.0.30
+MYSQL_CONFIG_PKG="mysql80-community-release"
 if [ -n "${1}" ]; then
   MYSQL_REPO_URL="${1}"
 fi
 if [ -n "${2}" ]; then
   MYSQL_OPERATOR_PYTHON_DEPS="${2}"
 fi
+if [ -n "${3}" ]; then
+ MYSQL_SHELL_VERSION="${3}"
+fi
+if [ -n "${4}" ]; then
+ MYSQL_CONFIG_PKG="${4}"
+fi
 sed 's#%%MYSQL_OPERATOR_PYTHON_DEPS%%#'"${MYSQL_OPERATOR_PYTHON_DEPS}:${MYSQL_OPERATOR_PYTHON_DEPS_VERSION}"'#g' docker-build/Dockerfile > tmpfile
 sed -i 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
 sed -i 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
+sed -i 's#%%MYSQL_CONFIG_PKG%%#'"${MYSQL_CONFIG_PKG}"'#g' tmpfile
 
 mv tmpfile Dockerfile

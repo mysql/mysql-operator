532e659c2d509b2f976d1b83d45222cccd7718da
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2022-06-04 13:34:32 +0200

    MYF-267: set up pipeline for building operator on jenkins
    - add monitor-trigger job due to issues with SCM polling within the pipeline job
      it is an extra job that monitors all pushes to the repo, discerns the branch, and optionally triggers a proper pipeline
    
    Change-Id: I72b49b22abd10cbc525d08c56ac0f1bcc0e864a6

---
+++ tests/ci/jobs/monitor-trigger-job.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Copyright (c) 2022, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+#
+# monitors development branches that are not watched at concourse
+# then triggers a build in a proper pipeline depending on the branch name
+# the git branches monitored at concourse are (in theory): dev, qa, itch, and trunk. In practice, it is trunk only,
+# but it shall change in the future, hopefully
+set -vx
+
+source $WORKSPACE/tests/ci/jobs/auxiliary/set-env.sh
+
+echo "GIT_COMMIT: ${GIT_COMMIT}"
+echo "GIT_COMMITTER_NAME: ${GIT_COMMITTER_NAME}"
+echo "GIT_COMMITTER_EMAIL: ${GIT_COMMITTER_EMAIL}"
+echo "GIT_URL: ${GIT_URL}"
+echo "GIT_URL_N: ${GIT_URL_N}"
+echo "GIT_BRANCH: ${GIT_BRANCH}"
+echo "GIT_LOCAL_BRANCH: ${GIT_LOCAL_BRANCH}"
+echo "GIT_PREVIOUS_COMMIT: ${GIT_PREVIOUS_COMMIT}"
+echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+
+# jenkins regex filter for dev branch: ":^(?!origin/trunk$|origin/itch$|origin/dev$|origin/qa$|origin/s?qa-.*$).+$"
+
+BRANCH_SHORT_NAME=${GIT_BRANCH#"origin/"}
+
+case "${BRANCH_SHORT_NAME}" in
+trunk)
+	# skip, should be triggered by concourse
+	;;
+itch)
+	JOB_NAME="itch"
+	;;
+dev)
+	JOB_NAME="dev"
+	;;
+qa)
+	JOB_NAME="qa"
+	;;
+qa-*|sqa*)
+	JOB_NAME="qa"
+	;;
+*)
+	JOB_NAME="dev"
+	;;
+esac
+
+if [[ -z $JOB_NAME ]]; then
+	echo "skipped ${GIT_BRANCH} $GIT_COMMIT"
+	exit 0
+fi
+
+OPERATOR_GIT_REVISION=$GIT_COMMIT
+OPERATOR_DEV_IMAGE_TAG='dev-'$(git describe --tags)
+OPERATOR_IMAGE=$LOCAL_REGISTRY_ADDRESS/$LOCAL_REPOSITORY_NAME/mysql-operator:$OPERATOR_DEV_IMAGE_TAG
+
+JOB_PARAMS="OPERATOR_GIT_REVISION=${OPERATOR_GIT_REVISION}&OPERATOR_IMAGE=${OPERATOR_IMAGE}&OPERATOR_INTERNAL_BUILD=true"
+
+JOB_NAME=test-pipeline
+JOB_LINK=${JOB_PREFIX}/${JOB_NAME}
+curl -X POST -u ${JENKINS_USER_CRED} ${JOB_LINK}/buildWithParameters?${JOB_PARAMS}

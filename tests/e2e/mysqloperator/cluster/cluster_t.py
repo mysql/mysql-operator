b239cbfb38e7de76b6e5abaa38adef32ce750da2
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2022-06-28 13:43:00 +0200

    random failures in Cluster3Defaults
    increase timeouts once again
    
    Change-Id: I481f188c822b69725e161373cf1139b791e2d132

--- tests/e2e/mysqloperator/cluster/cluster_t.py
+++ tests/e2e/mysqloperator/cluster/cluster_t.py
@@ -1083,7 +1083,7 @@ spec:
         # XXX deleting the sts shouldn't be necessary, but it's not happening when the ic is deleted
         kutil.delete_sts(self.ns, "mycluster")
 
-        kutil.delete_ic(self.ns, "mycluster", 500)
+        kutil.delete_ic(self.ns, "mycluster", 600)
 
         self.wait_pod_gone("mycluster-2")
         self.wait_pod_gone("mycluster-1")

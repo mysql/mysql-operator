b5e9a0dc3dadf8106e2fb5c1b7338e33bab90617
Andrey Hristov <andrey.hristov@oracle.com>
2022-07-08 18:55:28 +0300

    Backup code should log much less
    Change-Id: I01eed246f1e5a30bfc6baeb0ea5ce14c1d45d200

--- mysqloperator/backup_main.py
+++ mysqloperator/backup_main.py
@@ -310,7 +310,7 @@ def main(argv):
 
     debug = False
 
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - [%(levelname)s] [%(name)s] %(message)s',
                         datefmt="%Y-%m-%dT%H:%M:%S")
 

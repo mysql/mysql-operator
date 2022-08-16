fc7ec0ae1d3f00aaa7e0268fe672483308e657d8
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2022-08-16 23:02:09 +0200

    MYF-274: weekly builds for older k8s versions
    - update list of images for k3d v5.4.4
    
    Change-Id: I4d093d2cf95c5f07f4287e87d8386716d3f49598

--- tests/ci/k3d/preload-images-to-registry.sh
+++ tests/ci/k3d/preload-images-to-registry.sh
@@ -18,17 +18,21 @@
 # 3) docker network connect k3d-k3s-default registry.localhost
 # 4) observe until containers are running (or not):
 # a) kubectl get pods -Aw
-# b) kubectl get events -A | grep pulled (will list needed images)
-#    to recognize which images are missing
+# b) kubectl get events -A | grep pulled
+#	 it will list needed images, to recognize which images are missing
 # 5) then update below the IMAGES list accordingly and charge the local registry
 # 5a) the first images like rancher/k3d-proxy rancher/k3d-tools should be updated
 #     according to the proper version (e.g. 5.3.0)
 
 REGISTRY=registry.localhost:5000
 
+IMAGES_v5_1_0="rancher/k3d-proxy:5.1.0 rancher/k3d-tools:5.1.0 rancher/pause:3.1 rancher/coredns-coredns:1.8.3 rancher/metrics-server:v0.3.6 rancher/klipper-helm:v0.6.4-build20210813 rancher/local-path-provisioner:v0.0.19 rancher/library-traefik:2.4.8 rancher/klipper-lb:v0.2.0 rancher/library-busybox:1.32.1"
+
 IMAGES_v5_3_0="rancher/k3d-proxy:5.3.0 rancher/k3d-tools:5.3.0 rancher/pause:3.1 rancher/mirrored-coredns-coredns:1.8.6 rancher/mirrored-metrics-server:v0.5.2 rancher/klipper-helm:v0.6.6-build20211022 rancher/local-path-provisioner:v0.0.21 rancher/mirrored-library-traefik:2.5.6 rancher/klipper-lb:v0.3.4 rancher/library-busybox:1.32.1"
 
-IMAGES="rancher/k3d-proxy:5.1.0 rancher/k3d-tools:5.1.0 rancher/pause:3.1 rancher/coredns-coredns:1.8.3 rancher/metrics-server:v0.3.6 rancher/klipper-helm:v0.6.4-build20210813 rancher/local-path-provisioner:v0.0.19 rancher/library-traefik:2.4.8 rancher/klipper-lb:v0.2.0 rancher/library-busybox:1.32.1"
+IMAGES_v5_4_0="ghcr.io/k3d-io/k3d-proxy:5.4.4 ghcr.io/k3d-io/k3d-tools:5.4.4 rancher/pause:3.1 rancher/mirrored-coredns-coredns:1.9.1 rancher/mirrored-metrics-server:v0.5.2 rancher/klipper-helm:v0.7.3-build20220613 rancher/local-path-provisioner:v0.0.21 rancher/mirrored-library-traefik:2.6.2 rancher/klipper-lb:v0.3.5 rancher/library-busybox:1.32.1"
+
+IMAGES=$IMAGES_v5_4_0
 
 for IMAGE in $IMAGES; do
 	docker pull $IMAGE

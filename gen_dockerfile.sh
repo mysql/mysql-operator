a6466f5bc09f83336d3155c751b11ef90cb2ba73
Lars Tangvald <lars.tangvald@oracle.com>
2022-09-13 11:14:53 +0200

    Update build files to enable specified yum repository
    For weekly shell builds, we need to use the weekly-tools-community
    repo, which is disabled by default.

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -10,6 +10,7 @@ MYSQL_OPERATOR_PYTHON_DEPS="mysql-operator-python-deps"
 MYSQL_OPERATOR_PYTHON_DEPS_VERSION="3.9.5"
 MYSQL_SHELL_VERSION=8.0.31
 MYSQL_CONFIG_PKG="mysql80-community-release"
+MYSQL_SHELL_REPO="mysql-tools-community"
 if [ -n "${1}" ]; then
   MYSQL_REPO_URL="${1}"
 fi
@@ -22,9 +23,13 @@ fi
 if [ -n "${4}" ]; then
  MYSQL_CONFIG_PKG="${4}"
 fi
+if [ -n "${5}" ]; then
+ MYSQL_SHELL_REPO="${5}"
+fi
 sed 's#%%MYSQL_OPERATOR_PYTHON_DEPS%%#'"${MYSQL_OPERATOR_PYTHON_DEPS}:${MYSQL_OPERATOR_PYTHON_DEPS_VERSION}"'#g' docker-build/Dockerfile > tmpfile
 sed -i 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
 sed -i 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
 sed -i 's#%%MYSQL_CONFIG_PKG%%#'"${MYSQL_CONFIG_PKG}"'#g' tmpfile
+sed -i 's#%%MYSQL_SHELL_REPO%%#'"${MYSQL_SHELL_REPO}"'#g' tmpfile
 
 mv tmpfile Dockerfile

bd85a80732907a7257738ae5c05c66e0b773c53a
Johannes Schlüter <johannes.schlueter@oracle.com>
2023-05-03 15:26:12 +0200

    Bump to 8.0.33-2.0.10
    Change-Id: Icfbe7dd79eaa2d5d6fb13fe6996fd157513999d9

--- tag.sh
+++ tag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+# Copyright (c) 2021, 2023 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
 
-echo "8.0.33-2.0.9"
+echo "8.0.33-2.0.10"

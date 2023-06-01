83a663f7f77566989f9366aafa14359865b01c91
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2023-06-01 12:24:47 +0200

    restore CI env - local registry
    changes in scripts related to charging local registry from a linked image archive
    - pull and save a linked image archive only if a related file doesn't exist or is older
    - support adding a version suffix to an archive filename; it is useful in case the archive file
      name doesn't contain any distinguishing information like
      https://github.com/k3s-io/k3s/releases/download/v1.26.0%2Bk3s1/k3s-airgap-images-amd64.tar.gz
      where the version is mentioned in the link but not in its last element
    
    Change-Id: I223873f6ed0d4be86e64e879bfc94ec83ab1540c

--- tests/ci/restore/registry/charge-registry-from-link.sh
+++ tests/ci/restore/registry/charge-registry-from-link.sh
@@ -11,16 +11,21 @@
 # archives-dir - optional directory where archive is to be stored, if it contains the archive to be downloaded, then
 # 		the timestamp will be checked, and file will be downloaded only if newer
 # 		by default it is a temporary directory
+# archive-suffix - optional suffix to add to the filename of the archive, e.g.
+#		if link is https://github.com/k3s-io/k3s/releases/download/v1.23.15%2Bk3s1/k3s-airgap-images-amd64.tar.gz and
+# 		archive-suffix is v1.23.15_2Bk3s1, then the archive file will be k3s-airgap-images-amd64-v1.23.15_2Bk3s1.tar.gz,
+# 		i.e. suffix is added in front of the extension
+#
 # e.g.
 # ./charge-registry-from-link.sh https://github.com/k3s-io/k3s/releases/download/v1.23.15%2Bk3s1/k3s-airgap-images-amd64.tar.gz \
-# 		registry.localhost:5000 ~/k8s-archives
+# 		registry.localhost:5000 ~/k8s-archives v1.23.15_2Bk3s1
 # ./charge-registry-from-link.sh https://github.com/k3s-io/k3s/releases/download/v1.22.7%2Bk3s1/k3s-airgap-images-arm64.tar \
-# 		registry.localhost:5000 ./archives
+# 		registry.localhost:5000 ./archives v1.22.7_2Bk3s1
 
 set -vx
 
-if [[ "$#" -ne 2 && "$#" -ne 3 ]]; then
-	echo "usage: <image-archive-link> <registry-url> [archives-dir]"
+if [[ "$#" -ne 2 && "$#" -ne 3 && "$#" -ne 4 ]]; then
+	echo "usage: <image-archive-link> <registry-url> [archives-dir] [archive-suffix]"
 	exit 1
 fi
 
@@ -28,14 +33,26 @@ SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
 ARCHIVE_LINK=$1
 REGISTRY_URL=$2
-if [ "$#" -eq 3 ]; then
+if [[ "$#" -eq 3 || "$#" -eq 4 ]]; then
 	ARCHIVES_DIR=$3
 else
 	ARCHIVES_DIR=$(mktemp -d)
 fi
+if [[ "$#" -eq 4 ]]; then
+	ARCHIVE_SUFFIX=$4
+else
+	ARCHIVE_SUFFIX=
+fi
 
 ARCHIVE_FILENAME=$(basename $ARCHIVE_LINK)
-ARCHIVE_PATH=$ARCHIVES_DIR/$ARCHIVE_FILENAME
+if [[ -z $ARCHIVE_SUFFIX ]]; then
+	ARCHIVE_PATH=$ARCHIVES_DIR/$ARCHIVE_FILENAME
+else
+	ARCHIVE_BASEFILENAME="${ARCHIVE_FILENAME%%.*}"
+	ARCHIVE_EXTENSION="${ARCHIVE_FILENAME#*.}"
+	ARCHIVE_PATH=$ARCHIVES_DIR/${ARCHIVE_BASEFILENAME}-${ARCHIVE_SUFFIX}.${ARCHIVE_EXTENSION}
+fi
+
 curl -L ${ARCHIVE_LINK} -o ${ARCHIVE_PATH} --time-cond ${ARCHIVE_PATH}
 
 $SCRIPT_DIR/charge-registry-from-archive.sh $ARCHIVE_PATH $REGISTRY_URL
--- tests/ci/restore/registry/charge-registry-from-links.sh
+++ tests/ci/restore/registry/charge-registry-from-links.sh
@@ -5,20 +5,24 @@
 #
 
 # auxiliary script to charge a registry according to specified list of versions of image archives (also compressed)
-# usage: <list-of-versions-path> <src-link-pattern> <archives-dir> <registry-url>
+# usage: <list-of-versions-path> <src-link-pattern> <archives-dir> <registry-url> [add-suffix]
 # list-of-versions-path - text file path with list of image versions
 # src-link-pattern - pattern of link containing token VERSION to be substituted with the actual version, e.g.
 #   https://github.com/k3s-io/k3s/releases/download/VERSION/k3s-airgap-images-amd64.tar.gz
 # archives-dir - directory with images archives, archives will be downloaded only if newer
 # registry-url - url of a registry to be charged
+# add-suffix - optional flag, add the version as a suffix of an archive file name; useful in case the archive file
+# 		name doesn't contain any distinguishing information like
+# 		https://github.com/k3s-io/k3s/releases/download/v1.26.0%2Bk3s1/k3s-airgap-images-amd64.tar.gz
+# 		where the version is mentioned in the link but not in its last element
 # e.g.
 # ./charge-registry-from-links.sh ./k3d/k3s-airgap-images.txt \
-#       https://github.com/k3s-io/k3s/releases/download/VERSION/k3s-airgap-images-amd64.tar.gz ~/k8s-archives
+#       https://github.com/k3s-io/k3s/releases/download/VERSION/k3s-airgap-images-amd64.tar.gz ~/k8s-archives 1
 
 set -vx
 
-if [ "$#" -ne 4 ]; then
-	echo "usage: <list-of-versions-path> <src-link-pattern> <archives-dir> <registry-url>"
+if [[ "$#" -ne 4 && "$#" -ne 5 ]]; then
+	echo "usage: <list-of-versions-path> <src-link-pattern> <archives-dir> <registry-url> [add-suffix]"
 	exit 1
 fi
 
@@ -28,9 +32,15 @@ VERSIONS_LIST=$1
 SRC_LINK_PATTERN=$2
 ARCHIVES_DIR=$3
 REGISTRY_URL=$4
+if [ "$#" -eq 5 ]; then
+	ADD_SUFFIX=$5
+fi
 
 while read -r VERSION_TO_DOWNLOAD
 do
 	ARCHIVE_LINK=${SRC_LINK_PATTERN/VERSION/$VERSION_TO_DOWNLOAD}
-	$SCRIPT_DIR/charge-registry-from-link.sh $ARCHIVE_LINK $REGISTRY_URL $ARCHIVES_DIR
+	if [[ -n $ADD_SUFFIX ]]; then
+		ARCHIVE_SUFFIX=$(sed -e 's/[.:\@%/]/_/g' <<< $VERSION_TO_DOWNLOAD)
+	fi
+	$SCRIPT_DIR/charge-registry-from-link.sh $ARCHIVE_LINK $REGISTRY_URL $ARCHIVES_DIR $ARCHIVE_SUFFIX
 done < $VERSIONS_LIST

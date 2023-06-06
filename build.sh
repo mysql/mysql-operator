63b05087dbbdba3d7c1dab6cb7ad068fb1e6b42b
Prashant Tekriwal <prashant.tekriwal@oracle.com>
2023-06-06 12:47:56 +0200

    ET#77005 - Add support for ARM docker imags for Operator
    Change-Id: I0db9661070b0f94d014346eac74b3237704ab8a2

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
+ARCH='amd64'; [ -n "$1" ] && ARCH="${1}"
 set -e
 
-docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} -t mysql/community-operator:8.0 .
+docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} -t mysql/community-operator:8.0-$ARCH .
--- tag.sh
+++ tag.sh
@@ -4,5 +4,6 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
+SUFFIX=''; [ -n "$1" ] && ARCH="-${1}"
 
-echo "8.0.34-2.0.11"
+echo "8.0.34-2.0.11$ARCH"

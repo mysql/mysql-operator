e17ea23fb8f33574d70c5ec217e67eda84c18d1b
Darek Slusarczyk <dariusz.slusarczyk@oracle.com>
2023-06-13 19:53:44 +0200

    restore CI env (add job)
    add script for restore environment worker jenkins job
    
    Change-Id: I705472c93228fa70eb4d1ff7ca125334d96891f6

---
+++ tests/ci/jobs/restore-env-job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2023, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+#
+# an auxiliary script to help restore the environment - download needed binaries (kubectl, minikube, k3d, kind, ...),
+# ensure the local registry runs, and charge it with required images
+
+set -vx
+
+source $WORKSPACE/tests/ci/jobs/auxiliary/set-env.sh || exit 10
+
+BINARIES_DIR=$WORKSPACE/binaries
+ARCHIVES_DIR=$WORKSPACE/archives
+
+$CI_DIR/restore/restore-env.sh $BINARIES_DIR $ARCHIVES_DIR
+
+echo """
+    0. before executing this job, run pull-and-save-dockerhub-images.sh to collect unreachable images, then copy them to $ARCHIVES_DIR
+    1. after executing this job, copy all binaries from $BINARIES_DIR to a reachable path, e.g.
+        sudo cp -ruv $BINARIES_DIR /usr/local/bin
+"""

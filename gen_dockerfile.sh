1cfbce8f8cc7690e654da45054ae9f7f38b70aea
Johannes Schlüter <johannes.schlueter@oracle.com>
2023-09-25 15:33:57 +0200

    Upgrade Shell dependency
    Change-Id: I3503c5774172e3122f979f8c26472d798151320e

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -8,7 +8,7 @@
 MYSQL_REPO_URL="http://repo.mysql.com"; [ -n "${1}" ] && MYSQL_REPO_URL="${1}"
 MYSQL_OPERATOR_PYTHON_DEPS="mysql-operator-python-deps"; [ -n "${2}" ] && MYSQL_OPERATOR_PYTHON_DEPS="${2}"
 MYSQL_OPERATOR_PYTHON_DEPS_VERSION="3.10.8"; [ -n "${3}" ] && MYSQL_OPERATOR_PYTHON_DEPS_VERSION="${3}"
-MYSQL_SHELL_VERSION=8.0.34; [ -n "${4}" ] && MYSQL_SHELL_VERSION="${4}"
+MYSQL_SHELL_VERSION=8.0.35; [ -n "${4}" ] && MYSQL_SHELL_VERSION="${4}"
 MYSQL_CONFIG_PKG="mysql80-community-release"; [ -n "${5}" ] && MYSQL_CONFIG_PKG="${5}"
 MYSQL_SHELL_REPO="mysql-tools-community"; [ -n "${6}" ] && MYSQL_SHELL_REPO="${6}"
 ARCH="amd64"; [ -n "${7}" ] && ARCH="${7}"

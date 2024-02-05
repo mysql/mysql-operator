2effaa1c6b6db23a38f6afa19ce876df0f7371b7
Johannes Schlüter <johannes.schlueter@oracle.com>
2024-02-05 16:12:18 +0100

    Prioritize QA images over Weekly
    On CI QA images reference versions which are "good" for QA during
    release cycle. weekly images may be less predictable in state
    
    Change-Id: I8b5c3319ef58de375bf977a1719bfe5b3a3e4c2d

--- tests/ci/registry/ensure-local-registry-running.sh
+++ tests/ci/registry/ensure-local-registry-running.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023, Oracle and/or its affiliates.
+# Copyright (c) 2023, 2024 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
@@ -13,12 +13,13 @@ $CI_DIR/registry/run-local-registry.sh $LOCAL_REGISTRY_CONTAINER_NAME $LOCAL_REG
 IMAGES_LIST=$CI_DIR/registry/images-list.txt
 
 # charge the local registry
-$CI_DIR/registry/charge-local-registry.sh $REMOTE_REGISTRY_ADDRESS $REMOTE_REPOSITORY_NAME \
-	$LOCAL_REGISTRY_ADDRESS $LOCAL_REPOSITORY_NAME $IMAGES_LIST
-
 if [[ $OPERATOR_ALLOW_WEEKLY_IMAGES == 'true' ]]; then
 	# temporarily allow pulling not-so-stable router and server images from the weekly repository, but we need
 	# another policy for incremental images update (when they are good enough to not fail our test suite)
 	$CI_DIR/registry/charge-local-registry.sh $REMOTE_REGISTRY_ADDRESS $WEEKLY_REPOSITORY_NAME \
 		$LOCAL_REGISTRY_ADDRESS $LOCAL_REPOSITORY_NAME $IMAGES_LIST
 fi
+
+$CI_DIR/registry/charge-local-registry.sh $REMOTE_REGISTRY_ADDRESS $REMOTE_REPOSITORY_NAME \
+	$LOCAL_REGISTRY_ADDRESS $LOCAL_REPOSITORY_NAME $IMAGES_LIST
+

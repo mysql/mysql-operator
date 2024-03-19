3c6685e874e944b7e15d2d3474da257f7b41c54e
Johannes Schlüter <johannes.schlueter@oracle.com>
2024-03-19 14:16:09 +0100

    Fixes for latest Shell
    Change-Id: I153bcb87b01178772a5680ae6abce1cc8bfbc6fe

--- tests/e2e/mysqloperator/cluster/router_t.py
+++ tests/e2e/mysqloperator/cluster/router_t.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, 2023, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2024, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
@@ -22,7 +22,7 @@ def check_sidecar_health(test, ns, pod):
 
 def get_routing_options(ns, pod) -> dict:
         result = kutil.execp(ns, [pod, "sidecar"],
-                             ["mysqlsh", "root:sakila@localhost", "-e",
+                             ["mysqlsh", "root:sakila@localhost", "--js", "-e",
                               "print(dba.getCluster().routingOptions())",
                               "--quiet-start=2"])
         return json.loads(result)

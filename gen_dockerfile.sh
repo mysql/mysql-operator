3087a2b3acd8b70abf0b5779b2296128b83d7e9b
David Bates <13614128+imdebating@users.noreply.github.com>
2024-07-19 12:18:50 -0400

    update sed command to work on macos & linux & output variable values when running script.

--- gen_dockerfile.sh
+++ gen_dockerfile.sh
@@ -13,11 +13,24 @@ MYSQL_CONFIG_PKG="mysql80-community-release"; [ -n "${5}" ] && MYSQL_CONFIG_PKG=
 MYSQL_SHELL_REPO="mysql-tools-community"; [ -n "${6}" ] && MYSQL_SHELL_REPO="${6}"
 ARCH="amd64"; [ -n "${7}" ] && ARCH="${7}"
 
+echo "MYSQL_REPO_URL                     = $MYSQL_REPO_URL"
+echo "MYSQL_OPERATOR_PYTHON_DEPS         = $MYSQL_OPERATOR_PYTHON_DEPS"
+echo "MYSQL_OPERATOR_PYTHON_DEPS_VERSION = $MYSQL_OPERATOR_PYTHON_DEPS_VERSION"
+echo "MYSQL_SHELL_VERSION                = $MYSQL_SHELL_VERSION"
+echo "MYSQL_CONFIG_PKG                   = $MYSQL_CONFIG_PKG"
+echo "MYSQL_SHELL_REPO                   = $MYSQL_SHELL_REPO"
+echo "ARCH                               = $ARCH"
 
 sed 's#%%MYSQL_OPERATOR_PYTHON_DEPS%%#'"${MYSQL_OPERATOR_PYTHON_DEPS}:${MYSQL_OPERATOR_PYTHON_DEPS_VERSION}-${ARCH}"'#g' docker-build/Dockerfile > tmpfile
-sed -i 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
-sed -i 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
-sed -i 's#%%MYSQL_CONFIG_PKG%%#'"${MYSQL_CONFIG_PKG}"'#g' tmpfile
-sed -i 's#%%MYSQL_SHELL_REPO%%#'"${MYSQL_SHELL_REPO}"'#g' tmpfile
-
+if [[ $(uname) == "Darwin" ]]; then
+  sed -i '' 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
+  sed -i '' 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
+  sed -i '' 's#%%MYSQL_CONFIG_PKG%%#'"${MYSQL_CONFIG_PKG}"'#g' tmpfile
+  sed -i '' 's#%%MYSQL_SHELL_REPO%%#'"${MYSQL_SHELL_REPO}"'#g' tmpfile
+else
+  sed -i 's#%%MYSQL_SHELL_VERSION%%#'"${MYSQL_SHELL_VERSION}"'#g' tmpfile
+  sed -i 's#%%MYSQL_REPO_URL%%#'"${MYSQL_REPO_URL}"'#g' tmpfile
+  sed -i 's#%%MYSQL_CONFIG_PKG%%#'"${MYSQL_CONFIG_PKG}"'#g' tmpfile
+  sed -i 's#%%MYSQL_SHELL_REPO%%#'"${MYSQL_SHELL_REPO}"'#g' tmpfile
+fi
 mv tmpfile Dockerfile

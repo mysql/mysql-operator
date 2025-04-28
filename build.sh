ae7c0ae5bbcebcabcef74449619cdebd76b5ed09
Nawaz Nazeer Ahamed <nawaz.nazeer.ahamed@oracle.com>
2025-04-28 17:06:34 +0200

    ET#86125 - Operator: Hiding enterprise functionality
    * Updated build.sh to handle new Dockerfile.enterprise
    * Introduce DOCKERFILE and TAG variables with defaults
    * Add -f|--file <Dockerfile> and -t|--tag <image tag> option parsing
    * Update 'docker build' invocation

--- build.sh
+++ build.sh
@@ -4,10 +4,39 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
-ARCH='amd64'; [ -n "$1" ] && ARCH="${1}"
-set -e
+ARCH='amd64'
+if [[ $# -gt 0 && ! "$1" =~ ^- ]]; then
+  ARCH="$1"
+  shift
+fi
 
 IMG_TAG=$(./tag.sh)
 MAJOR_VERSION=${IMG_TAG:0:3}
+DOCKERFILE="Dockerfile"
 
-docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} -t mysql/community-operator:${MAJOR_VERSION}-$ARCH .
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -f|--file)
+      if [[ -n "${2-}" ]]; then
+        DOCKERFILE="$2"
+        shift 2
+      else
+        echo "Error: --file requires a filename argument" >&2
+        exit 1
+      fi
+      ;;
+    -t|--tag)
+      TAG="$2"
+      shift 2
+      ;;
+    -*)
+      echo "Unknown option: $1" >&2
+      exit 1
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+docker build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --build-arg no_proxy=${no_proxy} -f "${DOCKERFILE}" -t "${TAG}":${MAJOR_VERSION}-$ARCH .

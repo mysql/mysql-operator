23fa4beb9281e5ceb0559ec7ac012b7574958963
Johannes Schlüter <johannes.schlueter@oracle.com>
2025-10-13 14:21:55 +0200

    Bump version
    Change-Id: I44b2172fc66045a36f186bb87444a7d3678a8069

--- tag.sh
+++ tag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Copyright (c) 2021, 2024 Oracle and/or its affiliates.
+# Copyright (c) 2021, 2025 Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
 SUFFIX=''; [ -n "$1" ] && SUFFIX=${1}
 
-echo "8.4.7-2.1.9$SUFFIX"
+echo "8.4.8-2.1.10$SUFFIX"

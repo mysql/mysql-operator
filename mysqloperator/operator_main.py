d0483feb0a65f5b548c17de8a6d89e2a6eec4aba
Andrey Hristov <andrey.hristov@oracle.com>
2025-12-15 11:42:02 +0200

    Add logging for tracing parallelism issues
    Change-Id: I4f4d7dd78d620e0e6f64bab995139bc9e2de0646

--- mysqloperator/operator_main.py
+++ mysqloperator/operator_main.py
@@ -27,8 +27,12 @@ k8sobject.g_host = os.getenv("HOSTNAME")
 def main(argv):
     mysqlsh.globals.shell.options.useWizards = False
     # https://dev.mysql.com/doc/mysql-shell/8.0/en/mysql-shell-application-log.html
-    mysqlsh.globals.shell.options.logLevel = 4 # warning
+    mysqlsh.globals.shell.options.logLevel = 4 # 8 for tracing
     mysqlsh.globals.shell.options.verbose = 0
+
+    #mysqlsh.globals.shell.options.logFile= "/tmp/mysqlsh.log"
+    #mysqlsh.globals.shell.options.logLevel= 5
+    #mysqlsh.globals.shell.options.logSql= "all"
 
     myconfig.config_from_env()
 

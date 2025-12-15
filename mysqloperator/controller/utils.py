b70a1f48ac18d109ca9656ba434230166ffffcc2
Andrey Hristov <andrey.hristov@oracle.com>
2025-12-15 11:40:55 +0200

    Move away from setuptools (pkg_config) to importlib
    Change-Id: Id05a095c518151739e59f6b9f163206b71cdaa3f

--- mysqloperator/controller/utils.py
+++ mysqloperator/controller/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2020, 2025, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
@@ -12,6 +12,8 @@ import base64
 import threading
 import json
 import hashlib
+from logging import Logger
+import importlib.metadata
 
 from . import config
 
@@ -184,16 +186,14 @@ def indent(s: str, spaces: int) -> str:
     return ""
 
 
-def log_banner(path: str, logger) -> None:
-    import pkg_resources
+def log_banner(path: str, logger: Logger) -> None:
     from . import config
 
-    kopf_version = pkg_resources.get_distribution('kopf').version
+    kopf_version = importlib.metadata.version("kopf")
     ts = datetime.datetime.fromtimestamp(os.stat(path).st_mtime).isoformat()
 
     path = os.path.basename(path)
-    logger.info(
-        f"MySQL Operator/{path}={config.OPERATOR_VERSION} timestamp={ts} kopf={kopf_version} uid={os.getuid()}")
+    logger.info(f"MySQL Operator/{path}={config.OPERATOR_VERSION}  timestamp={ts}  kopf={kopf_version}  uid={os.getuid()}")
 
 
 def dict_to_json_string(d : dict) -> str:
